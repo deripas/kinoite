2e4f33518f6edc4093ddf959687493bfe75b30a5
Anton Deripaska <a.deripaska@semrush.com>
2026-07-13 13:48:17 +0200

    fix ostree

--- build_files/base/02-initramfs.sh
+++ build_files/base/02-initramfs.sh
@@ -5,7 +5,13 @@ set -eoux pipefail
 
 KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-longterm-core)
 
+# /root -> /var/roothome, а /var во время сборки — пустой tmpfs;
+# без каталога dracut-install падает с ERROR: installing '/root'
+mkdir -p /var/roothome
+
+# модуль ostree включается только по явному запросу (--add), без него
+# initramfs не выполнит ostree-prepare-root и switch-root в /sysroot упадёт
 export DRACUT_NO_XATTR=1
-dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v -f \
+dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f \
     "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
